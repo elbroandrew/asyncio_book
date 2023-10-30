b3ca1506c16a9162bab62c66db4bbbbb9adb19ed
andrew <elbroandrew@gmail.com>
2023-10-30 16:33:01 +1000

    GIL is released on I/O ops automatically

--- example-asyncio.py
+++ example-asyncio.py
@@ -14,10 +14,13 @@ start = time.time()
 
 t1.start()
 t2.start()
-
+print("print called between threads I/O execution.")
 t1.join()
 t2.join()
 
 end = time.time()
 
 print(f"Running synchronously: {end - start:.4f} secs.")
+
+# for I/O operations and some CPU bound operations the GIL is released automatically. See Martin Fowler book 
+# 'Python concurrency with asyncio', page 16.

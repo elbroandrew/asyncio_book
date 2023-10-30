85e507f2a8b59b90bf151f4f892b7b7820c93612
andrew <elbroandrew@gmail.com>
2023-10-30 16:25:20 +1000

    with threads

--- example-asyncio.py
+++ example-asyncio.py
@@ -1,15 +1,22 @@
 import time
 import requests
+import threading
 
 def read_example() -> None:
     response = requests.get("https://www.example.com")
     print(f"response status code: {response.status_code}")
 
+t1 = threading.Thread(target=read_example)
+t2 = threading.Thread(target=read_example)
+
 
 start = time.time()
 
-read_example()
-read_example()
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
 
 end = time.time()
 

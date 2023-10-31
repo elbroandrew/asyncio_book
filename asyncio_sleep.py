f4e245c5875ebd17723a59dcee2beeb5507e0a31
andrew <elbroandrew@gmail.com>
2023-10-31 18:16:02 +1000

    refactor: add delay into add_one method to see how it works in main func

--- asyncio_sleep.py
+++ asyncio_sleep.py
@@ -2,14 +2,16 @@ import asyncio
 from util import delay
 
 async def add_one(n: int) -> int:
+    await delay(2)
     return n + 1
 
 async def hello_there_coro() -> str:
-    await delay(1)
+    await delay(1)  # await puts everyithing on pause
     return 'hello world!'
 
 async def main():
     message = await hello_there_coro()
+    print("some message.") # this will happen after we 'awaited' the result from 'hello_there' func.
     one_plus_one = await add_one(1)
     print(one_plus_one)
     print(message)

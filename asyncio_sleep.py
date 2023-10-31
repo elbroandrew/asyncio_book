c2326b86f40f787ca174d2a7287d4b96e2d1a34a
andrew <elbroandrew@gmail.com>
2023-10-31 18:11:30 +1000

    refactor: add delay func into method and run concurrently

--- asyncio_sleep.py
+++ asyncio_sleep.py
@@ -1,12 +1,17 @@
 import asyncio
+from util import delay
 
+async def add_one(n: int) -> int:
+    return n + 1
 
 async def hello_there_coro() -> str:
-    await asyncio.sleep(2)
+    await delay(1)
     return 'hello world!'
 
 async def main():
     message = await hello_there_coro()
+    one_plus_one = await add_one(1)
+    print(one_plus_one)
     print(message)
 
 
